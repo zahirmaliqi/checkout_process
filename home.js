7585269b6809481fb9d9f5626c1fa44624d2675f
Zahir <zahir.maliqi@studentet.uni-pr.edu>
2022-11-27 04:28:29 -0300

    commit sub,vat and total

--- home.js
+++ home.js
@@ -4,7 +4,7 @@ const product =[
         image:'',
         name:'Bottle of Water',
         price: 0.25,
-        discount:0,
+        discount:0.1,
         VAT:8,
     },
     {
@@ -25,69 +25,78 @@ const product =[
     }
 ];
 
-const group = [...new Set(product.map((article)=>{
-  return article
-}))]
+const categories = [...new Set(product.map((item)=>{
+  return item}))]
 let i =0;
 
-document.getElementById('item-section').innerHTML=group.map((article)=>{
-    var{image,name,price} = article;
+document.getElementById('root').innerHTML=categories.map((item)=>{
+    var{image,name,price} = item;
     return(
-        `<div class="box">
-                <div class="image-part">
-                     <img class="img" src=${image}/>
+        `<div class='box'>
+                <div class='img-box'>
+                     <img class='imgages' src=${image}></img>
                 </div>
 
-                <div class="product-details">
+                <div class='bottom'>
                    <p> ${name} </p>
-                   <h3>$ ${price}</h3>
-                     `+ "<button onclick= 'addProducttoCart("+(i++)+")'>Add</button>"+
+                   <h2>$ ${price}</h2>`+ 
+                   "<button onclick='addtocart("+(i++)+")'>Add</button>"+
                 `</div>
           </div>`
     )
 }).join('')
 
 
-var bought_product=[];
+var cart=[];
 
-function addProducttoCart(k){
-    bought_product.push({...group[k]});
-    client_product();
-
+function addtocart(a){
+    cart.push({...categories[a]});
+    displaycart();
 }
 
-function client_product(k){
-    let a=0;
-    let sum=0;
-    document.getElementById("counter").innerHTML=bought_product.length;
-    if(bought_product.length == 0)
-    {
-        document.getElementById('emptyitem').innerHTML="Empty cart";
-        document.getElementById("total").innerHTML="$ 0.00";
-    } else{
-        document.getElementById('emptyitem').innerHTML=bought_product.map((article)=>{
-            
-                 var{image,name,price}=article;
-                 sum=sum+price;
-                 document.getElementById("total").innerHTML="$"+sum;
-                 return(
-                    `<div class='bought-product'>
-                         <div class='product-img'>
-                             <img class="productimage" alt=${name} src=${image}>
-                         </div>
-                         <p>${name}</p>
-                         <h3>$ ${price}</h3>`+
-                         "<img alt=${name} onclick='removeProduct("+ (a++) + ")'><div>"
-
-                 );
-        }).join('');
-    }
-}
-
-function removeProduct(k){
+function delElement(a){
     // bought_product.pop({...group[k]});
-    bought_product.splice(k,1);
-    client_product();
+    cart.splice(a,1);
+    displaycart();
 
 }
+
+      document.getElementById('cartItem').innerHTML="Empty cart";
+      document.getElementById("subTotal").innerHTML="$ 0.00";
+      document.getElementById("vat").innerHTML="$ 0.00";
+      document.getElementById("total").innerHTML="$ 0.00";
+
+function displaycart(a) {
+    let j = 0, subTotal = 0, general_vat = 0;
+
+    document.getElementById("count").innerHTML = cart.length;
+
+    if (cart.length !== 0){
+        document.getElementById('cartItem').innerHTML = cart.map((items) => {
+
+            var { image, name, price, discount, VAT } = items;
+
+            subTotal = subTotal + (price-discount);
+            general_vat = general_vat + ((price-discount) * (VAT / 100));
+            let total = general_vat + subTotal;
+
+            document.getElementById("subTotal").innerHTML = "$" + subTotal;
+            document.getElementById("vat").innerHTML = "$" + general_vat;
+            document.getElementById("total").innerHTML = "$" + total;
+
+            return (
+                `<div class='cart-item'>
+                         <div class='row-img'>
+                             <img class='rowing' alt=${name} src=${image}>
+                         </div>
+                         <p style="font-size:12px;">${name}</p>
+                         <h2 style="font-size:12px;">$ ${price}</h2>` +
+                "<img alt=${name} onclick='delElement(" + (j++) + ")'></img></div>"
+
+            );
+        }).join('');
+}
+}
+
+
 
