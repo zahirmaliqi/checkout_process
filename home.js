0a0c00714f8a161b7eeecbf138eb87f05f479d31
Zahir <zahir.maliqi@studentet.uni-pr.edu>
2022-11-28 02:28:50 -0300

    photos & style

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 const product =[
     {
         id:0,
-        image:'',
+        image:'./images/bottle.png',
         name:'Bottle of Water',
         price: 0.25,
         discount:0,
@@ -9,7 +9,7 @@ const product =[
     },
     {
         id:1,
-        image:'',
+        image:'./images/chip.png',
         name:'Chip',
         price: 2.40,
         discount:0,
@@ -17,7 +17,7 @@ const product =[
     },
     {
         id:2,
-        image:'',
+        image:'./images/tv.png',
         name:'TV',
         price: 760.00,
         discount:0,
@@ -25,11 +25,67 @@ const product =[
     },
     {
         id:3,
-        image:'',
+        image:'./images/coca.png',
         name:'CocaCola',
         price: 0.50,
         discount:0.10,
         VAT:18,
+    },
+    {
+        id:4,
+        image:'./images/choco.png',
+        name:'Chocolate bar',
+        price: 1.25,
+        discount:0,
+        VAT:22,
+    },
+    {
+        id:5,
+        image:'./images/soap.jpeg',
+        name:'Hand soap',
+        price: 3.78,
+        discount:0,
+        VAT:8,
+    },
+    {
+        id:6,
+        image:'./images/fish.png',
+        name:'Fish /kg',
+        price: 8.30,
+        discount:0,
+        VAT:18,
+    },
+    {
+        id:7,
+        image:'./images/humus.jpg',
+        name:'Humus /package',
+        price: 2.66,
+        discount:0,
+        VAT:18,
+    },
+    {
+        id:8,
+        image:'./images/white-wine.png',
+        name:'White Wine',
+        price:9.20,
+        discount:0.02,
+        VAT:18,
+    },
+    {
+        id:9,
+        image:'./images/banana.png',
+        name:'Banana /kg',
+        price: 1.25,
+        discount:0,
+        VAT:22,
+    },
+    {
+        id:10,
+        image:'./images/wine.png',
+        name:'Wine',
+        price: 9.78,
+        discount:0,
+        VAT:22,
     }
 ];
 
@@ -47,8 +103,8 @@ document.getElementById('root').innerHTML=categories.map((item)=>{
 
                 <div class='bottom'>
                    <p> ${name} </p>
-                   <h2>$ ${price}</h2>`+
-                   "<input type='number' min='1' name='' id='"+(k++)+"'>"+ 
+                   <h2>$ ${price.toFixed(2)}</h2>`+
+                   "<input class='quantity' placeholder='QTY' type='number' min='1' name='' id='"+(k++)+"'>"+ 
                    "<button onclick='addtocart("+(i++)+")'>Add</button>"+
                 `</div>
           </div>`
@@ -78,7 +134,7 @@ function delElement(a){
 function displaycart(a) {
     let j = 0, subTotal = 0, general_vat = 0;
 
-    document.getElementById("count").innerHTML = cart.length;
+    // document.getElementById("count").innerHTML = cart.length;
 
     if (cart.length !== 0){
         document.getElementById('cartItem').innerHTML = cart.map((items) => {
@@ -101,7 +157,7 @@ function displaycart(a) {
                          </div>
                          <p style="font-size:12px;">${name}</p>
                          <h2 style="font-size:12px;">${k} X $${price}</h2>` +
-                "<img alt=${name} onclick='delElement(" + (j++) + ")'></img></div>"
+                "<img src='./images/remove-icon.png' class='remove-bttn' onclick='delElement(" + (j++) + ")'></img></div>"
 
             );
         
